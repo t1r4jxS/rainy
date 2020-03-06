c0ce44bffffb30e09cb2fcadce17f57523b8512e
t1r4jxS <61889648+t1r4jxS@users.noreply.github.com>
2020-03-07 03:02:04 +0530

    Add files via upload

---
+++ jni/obj/local/armeabi-v7a/objs/apk/apk.o.d
@@ -0,0 +1,3 @@
+obj/local/armeabi-v7a/objs/apk/apk.o: jni/apk.cpp jni/MemoryTools.h
+
+jni/MemoryTools.h:
---
+++ jni/obj/local/x86/objs/apk/apk.o.d
@@ -0,0 +1,3 @@
+obj/local/x86/objs/apk/apk.o: jni/apk.cpp jni/MemoryTools.h
+
+jni/MemoryTools.h:
